00e213ff36ba9b389d2b90dfb155ab86d2bb942f
NSHALKEVICH <58821950+NSHALKEVICH@users.noreply.github.com>
2019-12-22 23:39:47 +0300

    Add files via upload

---
+++ calculatrice.py
@@ -0,0 +1,13 @@
+
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    return a/b
+def power(a, n):
+    return a**n
+def root(a, n):
+    return a**(1/n)
